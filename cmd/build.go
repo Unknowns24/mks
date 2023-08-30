6ec4c3fa55f1751ad4e29106c8870f10be5e459b
Unknowns24 <unknowns0074@gmail.com>
2023-08-30 00:57:40 -0300

    Added verbose mode to build cmd and root folder validation

--- cmd/build.go
+++ cmd/build.go
@@ -5,6 +5,10 @@ import (
 	"github.com/unknowns24/mks/manager"
 )
 
+var (
+	verbose bool // verbose flag
+)
+
 func NewBuildCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "build [name]",
@@ -13,10 +17,11 @@ func NewBuildCmd() *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			serviceName := args[0]
 			features, _ := cmd.Flags().GetStringSlice("features")
-			return manager.GenerateMicroservice(serviceName, features)
+			return manager.GenerateMicroservice(serviceName, verbose, features)
 		},
 	}
 
+	cmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose mode")
 	cmd.Flags().StringSlice("features", []string{}, "Features required for the microservice")
 
 	return cmd
--- manager/main.go
+++ manager/main.go
@@ -1,52 +1,85 @@
 package manager
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime"
+	"time"
 
 	"github.com/unknowns24/mks/utils"
 )
 
-func GenerateMicroservice(serviceName string, features []string) error {
+func GenerateMicroservice(serviceName string, verbose bool, features []string) error {
+	if verbose {
+		fmt.Println("[+] Creating " + serviceName + " microservice..")
+	}
+
 	// Get the current working directory
 	currentDir, err := os.Getwd()
 	if err != nil {
 		return err
 	}
 
+	if verbose {
+		fmt.Println("[+] Creating root folder..")
+	}
+
 	// Create the base path for the microservice
 	basePath := filepath.Join(currentDir, serviceName)
+
+	// Check if the microservice already was created
+	_, dirErr := os.Stat(basePath)
+	if !os.IsNotExist(dirErr) {
+		return errors.New(serviceName + " microservice already created!")
+	}
+
+	// Create base path directory
 	if err := os.MkdirAll(basePath, os.ModePerm); err != nil {
 		return err
 	}
 
-	createBaseFiles(basePath, serviceName)
+	if verbose {
+		time.Sleep(time.Second / 4) // sleep 250ms
+		fmt.Println("[+] Creating base files..")
+	}
 
-	// Check if "all" features are requested
-	if utils.SliceContainsElement(features, "all") {
-		// Add all available features to the microservice
-		err := AddAllFeatures(basePath)
-		if err != nil {
-			return err
+	createBaseFiles(basePath, serviceName, verbose)
+
+	// If features..
+	if len(features) >= 1 {
+		if verbose {
+			fmt.Println("[+] Checking requested features..")
 		}
-	} else {
-		// Add specified features to the microservice
-		for _, feature := range features {
-			err := AddFeature(basePath, feature)
+
+		// Check if "all" features are requested
+		if utils.SliceContainsElement(features, "all") {
+			// Add all available features to the microservice
+			err := AddAllFeatures(basePath)
 			if err != nil {
 				return err
 			}
+		} else {
+			// Add specified features to the microservice
+			for _, feature := range features {
+				err := AddFeature(basePath, feature)
+				if err != nil {
+					return err
+				}
+			}
 		}
+
+		fmt.Printf("[+] Microservice '%s' with features %v generated successfully!\n", serviceName, features)
+		return nil
 	}
 
-	fmt.Printf("Microservice '%s' with features %v generated successfully!\n", serviceName, features)
+	fmt.Printf("[+] Base Microservice '%s' generated successfully!\n", serviceName)
 	return nil
 }
 
-func createBaseFiles(basePath, serviceName string) error {
+func createBaseFiles(basePath, serviceName string, verbose bool) error {
 	// Get the directory path of the current file (generator.go)
 	_, filename, _, ok := runtime.Caller(0)
 	if !ok {
@@ -60,6 +93,10 @@ func createBaseFiles(basePath, serviceName string) error {
 	*************/
 
 	// Create main.go using template
+	if verbose {
+		fmt.Println("[+] Creating main.go file..")
+	}
+
 	mainTemplatePath := filepath.Join(mksDir, "..", "libs", "templates", "base", "main.go.template")
 	mainPath := filepath.Join(basePath, "main.go")
 
@@ -70,18 +107,33 @@ func createBaseFiles(basePath, serviceName string) error {
 	***************/
 
 	// Create the utils path for the required base files
+	if verbose {
+		time.Sleep(time.Second / 4) // sleep 250ms
+		fmt.Println("[+] Creating utils files directory..")
+	}
+
 	utilsPath := filepath.Join(basePath, "utils")
 	if err := os.MkdirAll(utilsPath, os.ModePerm); err != nil {
 		return err
 	}
 
 	// Create utils/config.go using template
+	if verbose {
+		time.Sleep(time.Second / 4) // sleep 250ms
+		fmt.Println("[+] Creating utils/config.go file..")
+	}
+
 	configTemplatePath := filepath.Join(mksDir, "..", "libs", "templates", "base", "utils", "config.go.template")
 	configPath := filepath.Join(utilsPath, "config.go")
 
 	utils.CreateFileFromTemplate(basePath, configTemplatePath, serviceName, configPath)
 
 	// Create utils/request.go using template
+	if verbose {
+		time.Sleep(time.Second / 4) // sleep 250ms
+		fmt.Println("[+] Creating utils/request.go file..")
+	}
+
 	requestTemplatePath := filepath.Join(mksDir, "..", "libs", "templates", "base", "utils", "request.go.template")
 	requestPath := filepath.Join(utilsPath, "request.go")
 
@@ -92,12 +144,22 @@ func createBaseFiles(basePath, serviceName string) error {
 	**************/
 
 	// Create the routes path for the required base files
+	if verbose {
+		time.Sleep(time.Second / 4) // sleep 250ms
+		fmt.Println("[+] Creating routes files directory..")
+	}
+
 	routesPath := filepath.Join(basePath, "routes")
 	if err := os.MkdirAll(routesPath, os.ModePerm); err != nil {
 		return err
 	}
 
 	// Create routes/mainRoutes.go using template
+	if verbose {
+		time.Sleep(time.Second / 4) // sleep 250ms
+		fmt.Println("[+] Creating routes/mainRoutes.go file..")
+	}
+
 	routesTemplatePath := filepath.Join(mksDir, "..", "libs", "templates", "base", "routes", "mainRoutes.go.template")
 	routesFilePath := filepath.Join(routesPath, "mainRoutes.go")
 
@@ -108,6 +170,11 @@ func createBaseFiles(basePath, serviceName string) error {
 	************/
 
 	// Execute "go mod init" command in the basePath directory
+	if verbose {
+		time.Sleep(time.Second / 4) // sleep 250ms
+		fmt.Println("[+] Running go mod init..")
+	}
+
 	initCmd := exec.Command("go", "mod", "init", serviceName)
 	initCmd.Dir = basePath
 	initOutput, err := initCmd.CombinedOutput()
@@ -115,6 +182,9 @@ func createBaseFiles(basePath, serviceName string) error {
 		return fmt.Errorf("error running 'go mod init': %s\nOutput: %s", err, initOutput)
 	}
 
-	fmt.Println("Base files created successfully.")
+	if verbose {
+		fmt.Println("[+] Base files created successfully.")
+	}
+
 	return nil
 }
