0af8fe8e9d7545b0a99b68921f1999ac0e8c4536
Unknowns24 <unknowns0074@gmail.com>
2023-09-02 15:34:34 -0300

    Created a new function to pass a dictionary with all placeholders and its values to change in a template

--- utils/files.go
+++ utils/files.go
@@ -4,8 +4,11 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/unknowns24/mks/config"
 )
 
+// Create files from templates wich only needs to changes %PACKAGE_NAME%
 func CreateFileFromTemplate(templatePath, serviceName, finalPath string) error {
 	// Read template content
 	templateContent, err := os.ReadFile(templatePath)
@@ -15,7 +18,31 @@ func CreateFileFromTemplate(templatePath, serviceName, finalPath string) error {
 	}
 
 	// Replace placeholders with the actual service name
-	fileContent := strings.ReplaceAll(string(templateContent), "%PACKAGE_NAME%", serviceName)
+	fileContent := strings.ReplaceAll(string(templateContent), config.PLACEHOLDER_PACKAGENAME, serviceName)
+
+	// Write the file content in the file
+	if err := os.WriteFile(finalPath, []byte(fileContent), os.ModePerm); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Create files from templates that has many placeholders to replace
+func CreateFileFromTemplateWithCustomReplace(templatePath, finalPath string, replaces map[string]string) error {
+	// Read template content
+	templateContent, err := os.ReadFile(templatePath)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	fileContent := string(templateContent)
+
+	// Replace every placeholders with its value
+	for flag, value := range replaces {
+		fileContent = strings.ReplaceAll(string(fileContent), flag, value)
+	}
 
 	// Write the file content in the file
 	if err := os.WriteFile(finalPath, []byte(fileContent), os.ModePerm); err != nil {
