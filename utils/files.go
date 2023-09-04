b024e1da943fef987e6a84a7121ecae9b88c59c8
unknowns <unknowns0074@gmail.com>
2023-09-04 16:40:05 -0300

    Implemented 3 functions to reuse on different addons installation

--- utils/files.go
+++ utils/files.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 
@@ -46,6 +47,55 @@ func CreateFileFromTemplateWithCustomReplace(templatePath, finalPath string, rep
 
 	// Write the file content in the file
 	if err := os.WriteFile(finalPath, []byte(fileContent), os.ModePerm); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Read file content and return it
+func ReadFile(filePath string) (string, error) {
+	// Read template content
+	templateContent, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	return string(templateContent), nil
+}
+
+// Read file content, replace placeholder with its values in memory and returns modified content
+func ReadFileWithCustomReplace(filePath string, replaces map[string]string) (string, error) {
+	// Read template content
+	templateContent, err := os.ReadFile(filePath)
+	if err != nil {
+		return "", err
+	}
+
+	fileContent := string(templateContent)
+
+	// Replace every placeholders with its value
+	for flag, value := range replaces {
+		fileContent = strings.ReplaceAll(string(fileContent), flag, value)
+	}
+
+	return fileContent, nil
+}
+
+// Function to extend file content by adding new content at the bottom of the file
+func ExtendFile(filePath, newContent string) error {
+	// Read the current content of the file
+	content, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+
+	// Insert the new configuration before the closing of the Config structure
+	finalContent := fmt.Sprintf("%s\n\n%s", content, newContent)
+
+	// Write the updated content to the env file
+	err = ioutil.WriteFile(filePath, []byte(finalContent), 0644)
+	if err != nil {
 		return err
 	}
 
