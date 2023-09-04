41afbe2491192a928ed173bd959a9fe8816e0dc6
unknowns <unknowns0074@gmail.com>
2023-09-04 16:35:57 -0300

    Added global folder with all global variables

---
+++ global/globals.go
@@ -0,0 +1,17 @@
+package global
+
+// Command global variables
+var (
+	Verbose bool // verbose flag for comands (Print what script is doing)
+)
+
+// Mks global variables
+var (
+	TemplatesFolderPath = "" // Path to templates folder inside mks
+)
+
+// Microservice global variables
+var (
+	BasePath    = "" // Current microservice inside path
+	ServiceName = "" // Current microservice module name
+)
