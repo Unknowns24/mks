28d74608c03c3896900b08fbf925d33042c720b6
unknowns <unknowns0074@gmail.com>
2023-09-04 18:06:05 -0300

    Improved ExtendFile function, import support added

--- utils/files.go
+++ utils/files.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"regexp"
 	"strings"
 
 	"github.com/unknowns24/mks/config"
@@ -90,8 +91,35 @@ func ExtendFile(filePath, newContent string) error {
 		return err
 	}
 
-	// Insert the new configuration before the closing of the Config structure
-	finalContent := fmt.Sprintf("%s\n\n%s", content, newContent)
+	finalContent := string(content)
+
+	// Check if the file extension need more imports
+	if strings.Contains(newContent, "import") {
+		mainImports := ExtractImports(finalContent)
+		newImports := ExtractImports(newContent)
+
+		// Parsing and creting new headers
+		finalHeader := ExtractPackageLine(finalContent)
+		finalHeader += "\nimport ("
+
+		for _, importLine := range mainImports {
+			finalHeader += "\n" + importLine
+		}
+
+		for _, importLine := range newImports {
+			finalHeader += "\n" + importLine
+		}
+
+		finalHeader += "\n)"
+
+		cleanMainContent := RemovePackageAndImports(finalContent)
+		cleanNewContent := RemovePackageAndImports(newContent)
+
+		// Creating final Content
+		finalContent = fmt.Sprintf("%s\n%s\n\n%s", finalHeader, cleanMainContent, cleanNewContent)
+	} else {
+		finalContent = fmt.Sprintf("%s\n\n%s", content, newContent)
+	}
 
 	// Write the updated content to the env file
 	err = ioutil.WriteFile(filePath, []byte(finalContent), 0644)
@@ -100,6 +128,47 @@ func ExtendFile(filePath, newContent string) error {
 	}
 
 	return nil
+}
+
+// ExtractPackageLine extracts the line containing the "package" declaration from Go code.
+func ExtractPackageLine(code string) string {
+	// Utiliza una expresión regular para encontrar la línea que comienza con "package" seguida de caracteres opcionales.
+	packageRegex := regexp.MustCompile(`(?m)^package.*\n`)
+	matches := packageRegex.FindStringSubmatch(code)
+
+	if len(matches) > 0 {
+		return matches[0]
+	}
+
+	return ""
+}
+
+// RemovePackageAndImports removes the package declaration and all import statements from Go code.
+func RemovePackageAndImports(code string) string {
+	// Remove the line starting with "package" followed by optional characters.
+	packageRegex := regexp.MustCompile(`(?m)^package.*\n`)
+	code = packageRegex.ReplaceAllString(code, "")
+
+	// Remove all lines starting with "import" followed by optional characters.
+	importRegex := regexp.MustCompile(`import\s+\(\s*([\s\S]*?)\s*\)`)
+	code = importRegex.ReplaceAllString(code, "")
+
+	return code
+}
+
+// ExtractImports extracts import statements from Go code.
+func ExtractImports(code string) []string {
+	imports := make([]string, 0)
+	// The regular expression finds all lines that start with "import," followed by optional characters,
+	// and ends with a semicolon or a newline.
+	regex := regexp.MustCompile(`import\s+\(\s*([\s\S]*?)\s*\)`)
+	matches := regex.FindAllStringSubmatch(code, -1)
+
+	for _, match := range matches {
+		imports = append(imports, match[1])
+	}
+
+	return imports
 }
 
 func findClosingBrace(lines []string, startIndex int) int {
