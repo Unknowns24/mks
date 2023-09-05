922f775b44bcc1786b32848e1eb81057c7879b3e
unknowns <unknowns0074@gmail.com>
2023-09-05 14:47:55 -0300

    Added temportal template folder as const

--- config/folders.go
+++ config/folders.go
@@ -2,10 +2,11 @@ package config
 
 // Mks folders
 const (
-	FOLDER_BASE      = "base"
-	FOLDER_ADDONS    = "addons"
-	FOLDER_OTHERS    = "others"
-	FOLDER_TEMPLATES = "templates"
+	FOLDER_BASE         = "base"
+	FOLDER_ADDONS       = "addons"
+	FOLDER_OTHERS       = "others"
+	FOLDER_TEMPLATES    = "templates"
+	FOLDER_TMP_TEMPLATE = "template"
 )
 
 // Shared folders
