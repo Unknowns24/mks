902726cf5090e94a236bdf1ae043360b854d5b68
unknowns <unknowns0074@gmail.com>
2023-09-04 21:47:53 -0300

    Removed all features, now features will be dinamic

--- config/features.go
+++ config/features.go
@@ -1,19 +1,3 @@
 package config
 
 const ALL_FEATURES = "all"
-
-const FEATURE_JWT = "jwt"
-const FEATURE_MYSQL = "mysql"
-const FEATURE_GRPC_CLIENT = "grpc-client"
-const FEATURE_GRPC_SERVER = "grpc-server"
-const FEATURE_RMQ_PRODUCER = "rmq-producer"
-const FEATURE_RMQ_CONSUMER = "rmq-consumer"
-
-var Features = [...]string{
-	FEATURE_JWT,
-	FEATURE_MYSQL,
-	FEATURE_GRPC_CLIENT,
-	FEATURE_GRPC_SERVER,
-	FEATURE_RMQ_PRODUCER,
-	FEATURE_RMQ_CONSUMER,
-}
