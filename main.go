1486296cca539d853b22a99360b202f945eed3b9
Unknowns24 <unknowns0074@gmail.com>
2023-09-08 14:13:36 -0300

    Making global setup on main

--- main.go
+++ main.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"github.com/unknowns24/mks/cmd"
+	"github.com/unknowns24/mks/utils"
 )
 
 func main() {
+	/***************************
+	* SETTING GLOBAL VARIABLES *
+	****************************/
+
+	err := utils.SetExecutablePath()
+	if err != nil {
+		panic(err)
+	}
+
+	err = utils.SetTemplatesFolderPathGlobal()
+	if err != nil {
+		panic(err)
+	}
+
+	err = utils.SetCurrentInstalledTemplates()
+	if err != nil {
+		panic(err)
+	}
+
+	err = utils.SetUserConfigFolderPath()
+	if err != nil {
+		panic(err)
+	}
+
+	/******************
+	* EXECUTING COBRA *
+	*******************/
 	cmd.Execute()
 }
