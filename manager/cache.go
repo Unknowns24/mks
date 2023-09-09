3982e1c5f1eb53e25792d8613b6e79dc7b1d6e80
Sergio B <serbice@gmail.com>
2023-09-09 21:05:42 +0200

    added cache manager

---
+++ manager/cache.go
@@ -0,0 +1,20 @@
+package manager
+
+//ClearCache is a function that clears the mks cache directory
+func ClearCacheAll() {
+	//TODO: Finish HIM (FATALITY!)
+}
+
+//ClearCache is a function that clears the mks cache directory
+func ClearCacheFiles() {
+	//TODO: Finish HIM (FATALITY!)
+}
+
+//ClearCache is a function that clears the mks cache directory
+func ClearCacheZip() {
+	//TODO: Finish HIM (FATALITY!)
+}
+
+func ClearCacheTemporals() {
+	//TODO: Finish HIM (FATALITY!)
+}
