17b3baf1483c4f71105fede41ad8f52f281eec7d
Unknowns24 <unknowns0074@gmail.com>
2023-09-09 18:55:40 -0300

    removed constants from global to moved to config/info

---
+++ config/info.go
@@ -0,0 +1,8 @@
+package config
+
+const (
+	MKS_Info_Version    = "v0.0.1"
+	MKS_Info_Author     = "SEUS Code (unknowns24 & SERBice)"
+	MKS_Info_License    = "MIT"
+	MKS_Info_Repository = "github.com/unknowns24/mks"
+)
