c36802a90ccb5c6c867da5eb8c28c5d35acdf9bc
Unknowns24 <unknowns0074@gmail.com>
2023-09-09 18:56:36 -0300

    Fixed bad global variables

--- cmd/info.go
+++ cmd/info.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/unknowns24/mks/config"
 	"github.com/unknowns24/mks/global"
 	"github.com/unknowns24/mks/manager"
 )
@@ -11,16 +12,16 @@ import (
 func InfoCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "info",
-		Short: "Show information abour mks and templates",
+		Short: "Show information about mks application and used paths",
 		Args:  cobra.ExactArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			//Show program information to user
 			fmt.Println("MKS - Golang application manager CLI")
 			fmt.Println("")
-			fmt.Println("Version: \n    ", global.MKS_Info_Version)
-			fmt.Println("Authors: \n    ", global.MKS_Info_Author)
-			fmt.Println("License: \n    ", global.MKS_Info_License)
-			fmt.Println("Repository: \n    ", global.MKS_Info_Repository)
+			fmt.Println("Version: \n    ", config.MKS_Info_Version)
+			fmt.Println("Authors: \n    ", config.MKS_Info_Author)
+			fmt.Println("License: \n    ", config.MKS_Info_License)
+			fmt.Println("Repository: \n    ", config.MKS_Info_Repository)
 			fmt.Println("")
 			fmt.Printf("User data directory: \n    %s\n", global.ConfigFolderPath)
 			fmt.Printf("User data temporals: \n    %s\n", global.TemporalsPath)
@@ -28,6 +29,7 @@ func InfoCmd() *cobra.Command {
 			fmt.Printf("User data templates file cache: \n    %s\n", global.TemplateCachePath)
 			fmt.Printf("User data templates installed: \n    %s\n", global.UserTemplatesFolderPath)
 			fmt.Println("")
+
 			manager.ListTemplate()
 			return nil
 		},
