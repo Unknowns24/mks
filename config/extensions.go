9b161ae869795657620e09d9f547f7183a35bfc7
unknowns <unknowns0074@gmail.com>
2023-09-11 00:15:28 -0300

    Added extends file extension to extensions constants

--- config/extensions.go
+++ config/extensions.go
@@ -5,6 +5,7 @@ const (
 	FILE_EXTENSION_GO        = ".go"
 	FILE_EXTENSION_ZIP       = ".zip"
 	FILE_EXTENSION_DEPENDS   = ".depends"
+	FILE_EXTENSION_EXTENDS   = ".extends"
 	FILE_EXTENSION_TEMPLATE  = ".template"
 	FILE_EXTENSION_GOCONFIG  = ".goconfig"
 	FILE_EXTENSION_ENVCONFIG = ".envconfig"
