5a3f6dd717ec304607ab2c3c3973b17c063d44d4
unknowns <unknowns0074@gmail.com>
2023-09-11 22:09:00 -0300

    Updated app name placeholder

--- config/placeholders.go
+++ config/placeholders.go
@@ -4,5 +4,4 @@ package config
 const PLACEHOLDER_PACKAGENAME = "%%PACKAGE_NAME%%"
 
 // General
-const PLACEHOLDER_APP_NAME = "%APP_NAME%"
-const PLACEHOLDER_APP_PORT = "%APP_PORT%"
+const PLACEHOLDER_APP_NAME = "%%APP_NAME%%"
